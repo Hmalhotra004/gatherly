0584a1b0dc913980aa07764e455e0abd6bcb3f6d
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-03-12 11:38:42 +0530

    log in page

---
+++ src/components/auth/AuthInput.tsx
@@ -0,0 +1,48 @@
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { ControllerFieldState, FieldValues } from "react-hook-form";
+
+interface AuthInputProps {
+  id: string;
+  type: string;
+  field: FieldValues;
+  disabled: boolean;
+  fieldState: ControllerFieldState;
+  placeholder?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  autoComplete?: string;
+}
+
+const AuthInput = ({
+  id,
+  type,
+  field,
+  disabled,
+  fieldState,
+  placeholder,
+  value,
+  onChange,
+  autoComplete,
+}: AuthInputProps) => {
+  return (
+    <Input
+      className={cn(
+        `form-input block w-full border-0 py-1.5 text-gray-900 shadow-sm 
+          ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus-visible:ring-2 
+        focus-visible:ring-inset focus-visible:ring-sky-600 sm:text-sm sm:leading-6 focus-visible:ring-offset-0`,
+        disabled && "opacity-50 cursor-default",
+        fieldState?.error && "focus-visible:ring-red-900"
+      )}
+      id={id}
+      type={type}
+      autoComplete={autoComplete ?? "off"}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      {...field}
+    />
+  );
+};
+
+export default AuthInput;
